0f1d9bc555f1f5b08e6cc32ef7cdcf461b8fe330
mc <m.cesena@web.de>
2023-05-18 16:20:15 +0200

    added_ 230518

--- push1.bat
+++ push1.bat
@@ -1,6 +1,6 @@
 git init
 set a1=%DATE:~-2,2%%DATE:~-7,2%%DATE:~-10,2%
-git checkout -b "%a1%"
+git checkout -b "%a1%b"
 git add .
 git commit -m "added_ "%a1%""
 git remote add origin https://github.com/aa3110/q2.git

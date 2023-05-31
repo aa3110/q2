6d11294ddfb7e99ece312f978957ecbd8d42c656
mc <m.cesena@web.de>
2023-05-31 17:36:02 +0200

    added_ 230531

---
+++ f.bat
@@ -0,0 +1,4 @@
+7z a "%date%".7z *
+copy "%date%".7z d:\backup\
+copy "%date%".7z f:\backup
+del "%date%".7z
---
+++ gitlap1.bat
@@ -0,0 +1,9 @@
+cd chart
+git init
+git checkout -b "chart"
+git add "biblio" "inout"
+git add "start"
+git commit -m "added_chart"
+git remote add origin https://gitlab.com/aa31101/python.git
+git push --force origin
+pause

780c697459aebce0f8a53e2fd4da95c324e44bba
mc <m.cesena@web.de>
2023-06-01 17:15:29 +0200

    added_ 230601

--- f.bat
+++ f.bat
@@ -1,4 +1,5 @@
-7z a "%date%".7z *
+7z a "%date%".7z chart\*.* alerts\*.*  -r
 copy "%date%".7z d:\backup\
 copy "%date%".7z f:\backup
 del "%date%".7z
+pause
---
+++ gitlap2.bat
@@ -0,0 +1,8 @@
+cd alerts
+git init
+git checkout -b "alerts"
+git add "media" "signal"
+git commit -m "added_alerts"
+git remote add origin https://gitlab.com/aa31101/python.git
+git push --force origin
+pause
